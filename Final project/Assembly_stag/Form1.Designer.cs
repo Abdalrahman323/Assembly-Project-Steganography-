af60724aaed4c91fa5323c7f909cdd3eec06a3e8
Abdalrhman A.ismail <abdalrhmann323@gmail.com>
2017-06-07 07:14:11 +0200

    Add files via upload

---
+++ Final project/Assembly_stag/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Assembly_stag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openTxtImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCurentImageAsItToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCurentImageAsAFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("imagePictureBox.Image")));
+            this.imagePictureBox.Location = new System.Drawing.Point(141, 12);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(575, 397);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagePictureBox.TabIndex = 5;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 39);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Enter your message";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 39);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(733, 25);
+            this.toolStrip2.TabIndex = 11;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(60, 22);
+            this.toolStripSplitButton1.Text = "Option";
+            this.toolStripSplitButton1.ButtonClick += new System.EventHandler(this.toolStripSplitButton1_ButtonClick);
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem1,
+            this.openTxtImageToolStripMenuItem});
+            this.openImageToolStripMenuItem.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder;
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openImageToolStripMenuItem.Text = "open Image";
+            // 
+            // openImageToolStripMenuItem1
+            // 
+            this.openImageToolStripMenuItem1.Name = "openImageToolStripMenuItem1";
+            this.openImageToolStripMenuItem1.Size = new System.Drawing.Size(153, 22);
+            this.openImageToolStripMenuItem1.Text = "open image";
+            this.openImageToolStripMenuItem1.Click += new System.EventHandler(this.openImageToolStripMenuItem1_Click);
+            // 
+            // openTxtImageToolStripMenuItem
+            // 
+            this.openTxtImageToolStripMenuItem.Name = "openTxtImageToolStripMenuItem";
+            this.openTxtImageToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.openTxtImageToolStripMenuItem.Text = "open txt image";
+            this.openTxtImageToolStripMenuItem.Click += new System.EventHandler(this.openTxtImageToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveCurentImageAsItToolStripMenuItem,
+            this.saveCurentImageAsAFileToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.saveToolStripMenuItem.Text = "save ";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveCurentImageAsItToolStripMenuItem
+            // 
+            this.saveCurentImageAsItToolStripMenuItem.Name = "saveCurentImageAsItToolStripMenuItem";
+            this.saveCurentImageAsItToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.saveCurentImageAsItToolStripMenuItem.Text = "save curent image as it";
+            this.saveCurentImageAsItToolStripMenuItem.Click += new System.EventHandler(this.saveCurentImageAsItToolStripMenuItem_Click);
+            // 
+            // saveCurentImageAsAFileToolStripMenuItem
+            // 
+            this.saveCurentImageAsAFileToolStripMenuItem.Name = "saveCurentImageAsAFileToolStripMenuItem";
+            this.saveCurentImageAsAFileToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.saveCurentImageAsAFileToolStripMenuItem.Text = "save curent image as a file";
+            this.saveCurentImageAsAFileToolStripMenuItem.Click += new System.EventHandler(this.saveCurentImageAsAFileToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 39);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "save encrypted image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(356, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 441);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imagePictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openTxtImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCurentImageAsItToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCurentImageAsAFileToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Final project/Assembly_stag/Pop_out.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Assembly_stag
+{
+    public partial class Pop_out : Form
+    {
+        FileStream fs_message;
+        StreamWriter sw_message;
+        public Pop_out()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (message.Text == "")
+                MessageBox.Show("you shoud enter a message");
+            else if(message.Text.Length>9900)
+            {
+                MessageBox.Show("the message you entered very larger");
+
+            }
+            else
+            {
+                MessageBox.Show("Are you sure you want to encrypt this message \n" + message.Text);
+
+                fs_message = new FileStream("text_of_message.txt", FileMode.Create);  // create if not found ,overwrite if found
+                sw_message = new StreamWriter(fs_message);
+                sw_message.Write(message.Text);
+
+                sw_message.Close();
+                fs_message.Close();
+                this.Close();
+            }
+
+        }
+
+        private void message_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
